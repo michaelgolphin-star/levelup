45b5ee8547be53c3da7c724cda60d663a6d50a82
michaelgolphin-star <michaelgolphin@gmail.com>
2026-02-02 18:05:50 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   root: "client",
+  plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3000",
-        changeOrigin: true
-      }
-    }
+        target: "http://127.0.0.1:8080",
+        changeOrigin: true,
+      },
+    },
   },
+
   build: {
-    outDir: "../client/dist",
-    emptyOutDir: true
-  }
+    // IMPORTANT: build frontend into /dist at project root
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
 });
