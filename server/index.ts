47cbff8eb1d111a885056874cc62ee69c65d7465
michaelgolphin-star <michaelgolphin@gmail.com>
2026-02-03 18:00:26 -0500

    Enable trust proxy in Express app

--- server/index.ts
+++ server/index.ts
@@ -15,6 +15,7 @@ dotenv.config();
 const PORT = Number(process.env.PORT || 8080);
 
 const app = express();
+app.set("trust proxy", 1);
 app.use(cors({ origin: true, credentials: true }));
 app.use(helmet());
 app.use(express.json({ limit: "1mb" }));
