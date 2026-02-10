ae461c515ac9d8fd63b7fd6caea8afb9e913ab51
michaelgolphin-star <michaelgolphin@gmail.com>
2026-02-09 20:01:18 -0500

    Update App.tsx

--- client/src/ui/App.tsx
+++ client/src/ui/App.tsx
@@ -1,40 +1,39 @@
 // client/src/ui/App.tsx (FULL REPLACEMENT)
 
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import LandingPage from "./LandingPage";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AuthPage from "./AuthPage";
 import DashboardPage from "./DashboardPage";
 import InviteAcceptPage from "./InviteAcceptPage";
 import OutletInboxPage from "./OutletInboxPage";
 import { OutletHomePage, OutletSessionPage } from "./OutletPage";
 import ResetPage from "./ResetPage";
+import VisibilityPage from "./VisibilityPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
         <Route path="/login" element={<AuthPage mode="login" />} />
         <Route path="/register" element={<AuthPage mode="register" />} />
 
         <Route path="/dashboard" element={<DashboardPage />} />
+
         <Route path="/invite/:token" element={<InviteAcceptPage />} />
 
-        {/* Inbox (quick list / nav) */}
-        <Route path="/outlet/inbox" element={<OutletInboxPage />} />
+        <Route path="/reset" element={<ResetPage />} />
 
-        {/* Counselor’s Office */}
         <Route path="/outlet" element={<OutletHomePage />} />
         <Route path="/outlet/:id" element={<OutletSessionPage />} />
 
-        {/* Password reset */}
-        <Route path="/reset" element={<ResetPage />} />
+        <Route path="/outlet-inbox" element={<OutletInboxPage />} />
 
-        {/* Fallback */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        {/* A2: mini doctrine page */}
+        <Route path="/visibility" element={<VisibilityPage />} />
+
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </BrowserRouter>
   );
